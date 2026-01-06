506fd1281d0241daf1101efb262d48085bed740c
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-01-06 08:01:46 +0530

    Cors error

--- utils/db.ts
+++ utils/db.ts
@@ -8,10 +8,14 @@ const DB_URL = process.env.DB_URL as string;
 if (!DB_URL) {
   throw new Error("Please provide DB_URL in the environment variables");
 }
+ 
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(DB_URL, {
+      serverSelectionTimeoutMS: 5000,
+      bufferCommands: false,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log("MongoDB connection error", error);
