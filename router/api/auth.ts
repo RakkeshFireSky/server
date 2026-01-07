aec60b4a4dc5f8452d1b9e9dd1b98565cf55b081
RakkeshFireSky <rakkeshkumar@firesky.cc>
2026-01-07 13:40:30 +0530

    Add log for check error

--- router/api/auth.ts
+++ router/api/auth.ts
@@ -98,8 +98,8 @@ router.get("/me", authMiddleware, async (req: Request, res: Response) => {
 router.post('/logout', (req: Request, res: Response) => {
     res.clearCookie('accessToken', {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax",
+        secure: true,
+        sameSite: "none",
         path: '/',
     });
     return res.status(200).json({message: "Logged out successfully"})
